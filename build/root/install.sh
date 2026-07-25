caf1ffdab79963db7814d3f1e59599c30cdf73a4
binhex <megalith01@gmail.com>
2026-07-25 11:55:53 +0100

    fix: add ncurses to suppress termcap database warning

--- build/root/install.sh
+++ build/root/install.sh
@@ -40,7 +40,7 @@ refresh.sh
 ####
 
 # define pacman packages
-pacman_packages="git python python-pip python-uv xorg-server-xvfb"
+pacman_packages="git python python-pip python-uv xorg-server-xvfb ncurses"
 
 # install compiled packages using pacman
 if [[ -n "${pacman_packages}" ]]; then
